2dcc98a57862af3326b3e870c0861e37c597f5b6
Brendan Roche <brendan@idealmedia.com.au>
2014-01-20 03:53:58 +1100

    hide form once form submitted successfully

--- build/app/views/form.blade.php
+++ build/app/views/form.blade.php
@@ -6,41 +6,38 @@
 
 @section('body')
 
-    @if ($success)
+    <div id="success" class="{{ $success ? '' : 'hidden'}}">
+        <p>Thanks for your feedback.</p>
+        <p>Someone will get back to you soon.</p>
+    </div>
 
-        <div id="success">
-            <p>Thanks for your feedback.</p>
-            <p>Someone will get back to you soon.</p>
-        </div>
-
-    @else
-
-        <div id="errors">
-            @foreach ($errors as $error)
-                <p>{{{ $error }}}</p>
-            @endforeach
-        </div>
-
-        {{ Form::open(array('url' => '/', 'id' => 'enquiryForm')) }}
-
-        @foreach ($fields as $name => $field)
-        <p><span class="{{ in_array('required', $field['attributes']) ? '' : 'hidden' }}">*</span> {{ Form::label($name, $field['label']) }}:
-            @if ($field['type'] == 'text')
-                {{ Form::text($name, Input::old($name), $field['attributes']) }}
-            @elseif ($field['type'] == 'email')
-                {{ Form::email($name, Input::old($name)) }}
-            @elseif ($field['type'] == 'textarea')
-                {{ Form::textarea($name, Input::old($name)) }}
-            @elseif ($field['type'] == 'select')
-                {{ Form::select($name, $field['options'], Input::old($name)) }}
-            @endif
-        </p>
+    <div id="formContainer" class="{{ $success ? 'hidden' : ''}}">
+    <div id="errors">
+        @foreach ($errors as $error)
+        <p>{{{ $error }}}</p>
         @endforeach
+    </div>
 
-        <p>
-            {{ Form::submit('Submit') }}
-        </p>
+    {{ Form::open(array('url' => '/', 'id' => 'enquiryForm')) }}
 
-        {{ Form::close() }}
-    @endif
+    @foreach ($fields as $name => $field)
+    <p><span class="{{ in_array('required', $field['attributes']) ? '' : 'hidden' }}">*</span> {{ Form::label($name, $field['label']) }}:
+        @if ($field['type'] == 'text')
+            {{ Form::text($name, Input::old($name), $field['attributes']) }}
+        @elseif ($field['type'] == 'email')
+            {{ Form::email($name, Input::old($name)) }}
+        @elseif ($field['type'] == 'textarea')
+            {{ Form::textarea($name, Input::old($name)) }}
+        @elseif ($field['type'] == 'select')
+            {{ Form::select($name, $field['options'], Input::old($name)) }}
+        @endif
+    </p>
+    @endforeach
+
+    <p>
+        {{ Form::submit('Submit') }}
+    </p>
+
+    {{ Form::close() }}
+    </div>
 @stop
