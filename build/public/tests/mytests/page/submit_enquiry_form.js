976bea419d70dacce05086b4a0e0289a817a1854
Brendan Roche <brendan@idealmedia.com.au>
2014-01-20 11:14:48 +1100

    Fix unit test for checking form validity if required fields are empty

--- build/public/tests/mytests/page/submit_enquiry_form.js
+++ build/public/tests/mytests/page/submit_enquiry_form.js
@@ -1,10 +1,9 @@
 registerTest ('Submit Enquiry form test', {
     setup: function() {
-        this.reqFields = ['firstName', 'surname', 'email', 'daytimeContactNumber', 'address', 'suburb', 'state', 'postcode', 'enquiryType'];
+        this.reqFields = ['firstName', 'surname', 'email', 'daytimeContactNumber', 'address', 'suburb', 'postcode'];
         this.complaintReqFields = ['productName', 'productSize', 'useByDate', 'batchCode'];
         this.doc = this.workspace.document;
         this.form = this.workspace.document.getElementById("enquiryForm");
-        this.successContainer = this.workspace.document.getElementById("success");
     },
     load: function() {
         var self = this;
@@ -14,30 +13,46 @@ registerTest ('Submit Enquiry form test', {
             ok(this.form,'An element with the id "enquiryForm" exists.');
         })
 
-        .test ("Can form be submitted if any of the required fields are empty?",function($) {
+        .test ("Can form be submitted if any of the required fields are empty?",function() {
+            var field;
+            // clear any fields loaded by session data
             for(var i = 0; i < this.reqFields.length; i++) {
-                //$('#enquiryForm', this.doc).submit();
-                equal(this.form.submit(), false, 'Form should not be allowed to submit if any required fields are empty');
-                var field = this.reqFields[i];
+                field = this.reqFields[i];
+                this.doc.getElementById(field).value = '';
+            }
+
+            for(i = 0; i < this.reqFields.length; i++) {
+                field = this.reqFields[i];
+                equal(this.form.checkValidity(), false, 'Form should not be allowed to submit if any required fields are empty');
                 this.doc.getElementById(field).value = 'not empty';
             }
 
-            equal(this.form.submit(), true, 'Form should be allowed to submit if all required fields are not empty');
+            equal(this.form.checkValidity(), true, 'Form should be allowed to submit if all required fields are not empty');
         })
 
         .test ("Can form be submitted if any of the product required fields are empty and enquiry type is a 'Product complaint'?",function($) {
-            for(var i = 0; i < this.reqFields.length; i++) {
-                var field = this.reqFields[i];
-                this.doc.getElementById(field).value = 'not empty';
-            }
-            this.doc.getElementById('enquiryType').value = 'Product complaint';
+            var field;
+
+            $('#enquiryType', this.doc).val('Product complaint').change();
+
+            // clear any fields loaded by session data
             for(var i = 0; i < this.complaintReqFields.length; i++) {
-                equal(this.form.submit(), false, 'Form should not be allowed to submit if any product required fields are empty are enquiry type is a complaint');
-                var field = this.complaintReqFields[i];
+                field = this.complaintReqFields[i];
+                this.doc.getElementById(field).value = '';
+            }
+
+            for(i = 0; i < this.reqFields.length; i++) {
+                field = this.reqFields[i];
                 this.doc.getElementById(field).value = 'not empty';
             }
 
-            equal(this.form.submit(), true, 'Form should be allowed to submit if all product required fields are not empty and enquiry type is a complaint');
+            for(i = 0; i < this.complaintReqFields.length; i++) {
+                equal(this.form.checkValidity(), false, 'Form should not be allowed to submit if any product required fields are empty are enquiry type is a complaint');
+                field = this.complaintReqFields[i];
+                this.doc.getElementById(field).value = 'not empty';
+            }
+
+            equal(this.form.checkValidity(), true, 'Form should be allowed to submit if all product required fields are not empty and enquiry type is a complaint');
         });
 
     }
